3c3b291e5d4f7e2804ef4921f7c3ace6ced2cf2f
TJ <cooploops@gmail.com>
2017-10-19 22:00:13 -0700

    initiated git repo and added base files for homework 10

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: "fCUUJgG3zU9kDHg113hyJ8y4L",
+    consumer_secret: "T0SGUegkJHtnUvr3E7rtYat0aoWk3PcxytBKPch8M3PdtX8hal",
+    access_token_key: "848710993915936768-1ILb0DdOBwakdXp3E9AbiSXUhqzUgyy",
+    access_token_secret: "sK2Ph0uxW1cDyMOZZWLChnblFWbMr8iYvUJF1BF61IdGP"
+}
+
+module.exports = twitterKeys;
